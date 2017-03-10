6865153f196e7788419219eb014a789f30779b6f
n3vrax <n3vrax@gmail.com>
2017-03-11 00:37:15 +0200

    Updated php file headers doc blocks

--- src/FormsPlugin.php
+++ src/FormsPlugin.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * @copyright: DotKernel
- * @library: dot-controller-plugin-forms
- * @author: n3vra
- * Date: 1/25/2017
- * Time: 2:36 PM
+ * @see https://github.com/dotkernel/dot-controller-plugin-forms/ for the canonical source repository
+ * @copyright Copyright (c) 2017 Apidemia (https://www.apidemia.com)
+ * @license https://github.com/dotkernel/dot-controller-plugin-forms/blob/master/LICENSE.md MIT License
  */
 
 declare(strict_types = 1);
